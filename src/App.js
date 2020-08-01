f1d16b0397d55f86cfbfff4c882560ae98a7daac
dinek <wilk.dinek@gmail.com>
2020-08-01 17:52:05 +0200

    firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core'
+
 import { ListContainer } from './pages/list/listContainer'
 import { AddFormContainer } from "./pages/addForm/addFormContainer"
 import { EditFormContainer } from "./pages/editForm/editFormContainer"
-import { ThemeProvider } from '@material-ui/core'
 import { theme } from './theme'
 
 export const App = () => {
